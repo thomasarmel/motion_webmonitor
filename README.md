857a1382db566f034fddfd94fc200902b3b02b14
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-05-21 18:05:42 +0200

    Refactor code

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # motion_webmonitor
 
-If you are using motion with one or more Raspberry Pi (or whatever) to monitor your home, you should have noticed that the web interface is really not great (there isn't even HTTPS).
-The objective is therefore to make it possible to use a "normal" web interface in order to manage one or more cameras.
+If you are using motion with one or more Raspberry Pi (or whatever) to monitor your home, you should have noticed that
+the web interface is really not great (there isn't even HTTPS). The objective is therefore to make it possible to use
+a "normal" web interface in order to manage one or more cameras.
 
 ### How to install ?
 
--- public/script/script.js
+++ public/script/script.js
@@ -2,41 +2,30 @@ var form = document.forms["formConnect"];
 var champLogin = document.forms["formConnect"]["login"];
 var champPassword = document.forms["formConnect"]["password"];
 
-function validateForm()
- {  
-  if(champLogin.value === "")
-   {
-    champLogin.classList.remove("correct");
-    champLogin.classList.add("incorrect");
-    return false;
-  }
-  else if(champPassword.value === "")
-   {
-    champPassword.classList.remove("correct");
-    champPassword.classList.add("incorrect");
-    return false;
-  }
-                 
-  else
-   {
-    return true;
-  }
+function validateForm() {
+    if (champLogin.value === "") {
+        champLogin.classList.remove("correct");
+        champLogin.classList.add("incorrect");
+        return false;
+    } else if (champPassword.value === "") {
+        champPassword.classList.remove("correct");
+        champPassword.classList.add("incorrect");
+        return false;
+    } else {
+        return true;
+    }
 }
 
-form.addEventListener("blur", function(event)
- {
-  if(event.target.value === "")
-   {
-    event.target.classList.remove("correct");
-    event.target.classList.add("incorrect");
-  }
-  else
-   {
-    event.target.classList.remove("incorrect");
-    event.target.classList.add("correct");
-  }
+form.addEventListener("blur", function (event) {
+    if (event.target.value === "") {
+        event.target.classList.remove("correct");
+        event.target.classList.add("incorrect");
+    } else {
+        event.target.classList.remove("incorrect");
+        event.target.classList.add("correct");
+    }
 }, true);
 
-erreurConnect.addEventListener("click", function() {
-  erreurConnect.style.display="none";
+erreurConnect.addEventListener("click", function () {
+    erreurConnect.style.display = "none";
 });
