2e7d0a21ba3e38b729dc33f69487557aad54b441
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-05-27 16:05:44 +0200

    TLS compatibility improvement

--- motion_webmonitor.go
+++ motion_webmonitor.go
@@ -59,16 +59,6 @@ func main() {
 			MinVersion:               tls.VersionTLS12,
 			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 			PreferServerCipherSuites: true,
-			CipherSuites: []uint16{
-				tls.TLS_CHACHA20_POLY1305_SHA256,
-				tls.TLS_AES_128_GCM_SHA256,
-				tls.TLS_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				//tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				//tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-			},
 		}
 		m := autocert.Manager{
 			Prompt:     autocert.AcceptTOS,
